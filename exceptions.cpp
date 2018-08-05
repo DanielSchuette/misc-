bcf754c4bfe81f09c2d07e1dde0f5ffc8c0c912d
Daniel Schuette <d.schuette@online.de>
2018-08-05 19:00:09 -0400

    update exceptions.cpp with comments

--- exceptions.cpp
+++ exceptions.cpp
@@ -1,3 +1,6 @@
+/*
+ * A little program demonstrating how a try - catch construct might look like in C++
+ */
 #include <vector>
 #include <iostream>
 #include <string>
@@ -8,19 +11,15 @@ std::string begin_end_delimiter(50, '*');
 int main()
 {
 	std::cout << std::endl << begin_end_delimiter << std::endl;
-	try{
+	try {
 	    array.at(1000) = 0;
 	}
-	catch(std::out_of_range o){
-	    if(o.what()){
+    // Exception should be caught by reference, though
+	catch (std::out_of_range o) {
+	    if (o.what()) {
 	    	std::cout << "Exception during vector access attempt. 'exception.what()' = " << o.what() << std::endl;
 	    }
 	}
-
 	std::cout << begin_end_delimiter << std::endl << std::endl;
-	cout ;
-	sdfj
-
-
 	return 0;
 }
