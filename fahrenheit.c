5494ca0700e1b88f3176d73a20b9e4ae4b34c853
Daniel Schuette <d.schuette@online.de>
2018-09-25 20:23:27 +0200

    add floating point version of fahrenheit.c from K&R

--- fahrenheit.c
+++ fahrenheit.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#define FAHRENHEIT_FACTOR (5.0/9.0)
 
 /* function declarations */
-int fahrenheit(int x);
+float fahrenheit(float x);
 
 int main() {
     /* define integer type counter variables */
-    int start, end, step, current;
-    start = 0;
-    end = 300;
-    step = 20;
+    float start, end, step, current;
+    start = 0.0;
+    end = 300.0;
+    step = 20.0;
     current = start;
 
     while (current <= end) {
-        printf("%d\t%d\n", current, fahrenheit(current));
+        printf("%3.0f %6.1f\n", current, fahrenheit(current));
         current += step;
     }
     return 0;
 }
 
-int fahrenheit(int x) {
-    return (5.0/9.0)*(x-32);
+float fahrenheit(float x) {
+    return FAHRENHEIT_FACTOR * (x-32.0);
 }
